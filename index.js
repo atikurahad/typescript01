9415f658dbb2d7c0bc8224ff9f0fa158ba38fd65
unknown <atikurahad@gmail.com>
2024-10-29 19:54:33 +0600

    hands on array

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("object");
+var users;
+users = [];
+var user1;
+user1 = { name: "Tamim", age: 34 };
+users.push(user1);
+var user2;
+user2 = { name: "Amir", age: 24 };
+users.push(user2);
+var user3;
+user3 = { name: "Tanjim", age: 22 };
+users.push(user3);
+// console.log(users);
+for (var key in users) {
+    console.log(users[key]['name'], users[key]['age']);
+}
--- index.ts
+++ index.ts
@@ -1,1 +1,23 @@
-console.log("object");
+let users: object[];
+
+users = [];
+
+let user1: { name: string; age: number };
+
+user1 = { name: "Tamim", age: 34 };
+users.push(user1);
+
+let user2: { name: string; age: number };
+user2 = { name: "Amir", age: 24 };
+users.push(user2);
+
+let user3: { name: string; age: number };
+user3 = { name: "Tanjim", age: 22 };
+users.push(user3);
+
+// console.log(users);
+
+
+for (const key in users) {
+    console.log(users[key]['name'] , users[key]['age']);
+  }
